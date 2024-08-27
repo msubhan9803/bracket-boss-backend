febfcce14a0c0b7a3c8b57c81d04034fd04aa64b
msubhan9803 <mohammadsubhan9803@gmail.com>
2024-08-28 03:10:15 +0500

    feat: Config module setup done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validateEnvVariables } from './config/env.validation';
+import configuration from './config/configuration';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      validate: validateEnvVariables,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  const port = config.get<number>('PORT', 4000);
+
+  await app
+    .listen(port)
+    .then(() => console.log(`🚀 App running at: http://localhost:${port}`));
 }
 bootstrap();
