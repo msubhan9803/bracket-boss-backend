968dda46ef261b1ffe61766a380ca624772f92e5
msubhan9803 <mohammadsubhan9803@gmail.com>
2024-09-04 11:48:39 +0500

    feat: logger support added

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'log'],
+  });
   const config = app.get(ConfigService);
   const port = config.get<number>('APP_PORT', 4000);
 
