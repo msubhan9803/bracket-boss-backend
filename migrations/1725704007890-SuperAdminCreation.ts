30c5603eefc48e4c047c837304bc8ec8e91e9d40
msubhan9803 <mohammadsubhan9803@gmail.com>
2024-09-07 17:07:58 +0500

    feat: Both migrations scripts updated

---
+++ migrations/1725704007890-SuperAdminCreation.ts
@@ -0,0 +1,68 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { config } from 'dotenv';
+
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
+config({ path: envFilePath });
+
+export class SuperAdminCreation1725706999572 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Reset the sequence of the primary key (assuming the primary key is 'id')
+    await queryRunner.query(
+      `SELECT setval(pg_get_serial_sequence('user', 'id'), COALESCE(MAX(id), 0) + 1, false) FROM "user";`,
+    );
+
+    // Create a super admin role if it doesn't already exist
+    await queryRunner.query(`
+      INSERT INTO "role" ("name", "createdDate", "updatedDate")
+      VALUES ('Super Admin', NOW(), NOW())
+      ON CONFLICT (name) DO NOTHING;
+    `);
+
+    // Fetch the Super Admin role ID
+    const superAdminRole = await queryRunner.query(`
+      SELECT id FROM "role" WHERE name = 'Super Admin';
+    `);
+
+    const superAdminRoleId = superAdminRole[0].id;
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(
+      process.env.APP_SUPER_ADMIN_PASSWORD,
+      10,
+    );
+
+    // Insert a super admin user
+    await queryRunner.query(`
+      INSERT INTO "user" (name, email, password, created_at, updated_at)
+      VALUES ('Super Admin', '${process.env.APP_SUPER_ADMIN_EMAIL}', '${hashedPassword}', NOW(), NOW())
+      ON CONFLICT (email) DO NOTHING;
+    `);
+
+    // Fetch the newly created super admin user ID
+    const superAdminUser = await queryRunner.query(`
+      SELECT id FROM "user" WHERE email = '${process.env.APP_SUPER_ADMIN_EMAIL}';
+    `);
+
+    const superAdminUserId = superAdminUser[0].id;
+
+    // Assign the super admin role to the super admin user
+    await queryRunner.query(`
+      INSERT INTO "roles_users" ("roleId", "userId")
+      VALUES ('${superAdminRoleId}', '${superAdminUserId}')
+      ON CONFLICT DO NOTHING;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      DELETE FROM "roles_users" WHERE "userId" IN (
+        SELECT id FROM public.user WHERE email = '${process.env.APP_SUPER_ADMIN_EMAIL}'
+      );
+    `);
+
+    await queryRunner.query(`
+      DELETE FROM "user" WHERE "email" = '${process.env.APP_SUPER_ADMIN_EMAIL}';
+    `);
+  }
+}
--- migrations/1725704007897-SetRoles.ts
+++ migrations/1725704007897-SetRoles.ts
@@ -2,6 +2,11 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class SetRoles1725704007897 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    // Reset the sequence of the primary key (assuming the primary key is 'id')
+    await queryRunner.query(
+      `SELECT setval(pg_get_serial_sequence('role', 'id'), COALESCE(MAX(id), 0) + 1, false) FROM "role";`,
+    );
+
     // Predefined roles into the 'role' table
     await queryRunner.query(`
             INSERT INTO "role" ("name", "createdDate", "updatedDate") VALUES 
