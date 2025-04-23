22c63749cba36f3ef62b22363b6a81673af6bd3b
msubhan9803 <mohammadsubhan9803@gmail.com>
2025-04-23 06:37:05 +0500

    feat: Level Standings entity added;

---
+++ src/level/entities/levelStandings.entity.ts
@@ -0,0 +1,71 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { CustomNumberIdScalar } from 'src/common/scalars/custom-number-id.scalar';
+import { Level } from './level.entity';
+import { Team } from 'src/team-management/entities/team.entity';
+
+@ObjectType()
+@Entity()
+export class LevelTeamStanding {
+  @Field(() => CustomNumberIdScalar)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToOne(() => Level)
+  @JoinColumn()
+  @Field(() => Level)
+  level: Level;
+
+  @OneToOne(() => Team)
+  @JoinColumn()
+  @Field(() => Team)
+  team: Team;
+
+  @Field()
+  @Column('int')
+  wins: number;
+
+  @Field()
+  @Column('int')
+  losses: number;
+
+  @Field()
+  @Column('int')
+  pointsScored: number;
+
+  @Field()
+  @Column('int')
+  pointsAgainst: number;
+
+  @Field()
+  @Column('int')
+  pointsScoredByNumberOfGames: number;
+
+  @Field()
+  @Column('int')
+  pointsAgainstByNumberOfGames: number;
+
+  @Field()
+  @Column('int')
+  pointDiffByNumberOfGames: number;
+
+  @Field()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Field()
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor(levelTeamStanding: Partial<LevelTeamStanding>) {
+    Object.assign(this, levelTeamStanding);
+  }
+}
--- src/level/level.module.ts
+++ src/level/level.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Level } from './entities/level.entity';
 import { LevelTeam } from './entities/level-team.entity';
+import { LevelTeamStanding } from './entities/levelStandings.entity';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([
             Level,
-            LevelTeam
+            LevelTeam,
+            LevelTeamStanding
         ])
     ]
 })
