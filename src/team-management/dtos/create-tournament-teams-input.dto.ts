f27ed215b73355358934fa55e0550ef482b8bd2c
msubhan9803 <mohammadsubhan9803@gmail.com>
2025-04-24 16:28:44 +0500

    feat: [x] Fix duplicate team issue [createTournamentTeam]

--- src/team-management/dtos/create-tournament-teams-input.dto.ts
+++ src/team-management/dtos/create-tournament-teams-input.dto.ts
@@ -19,6 +19,6 @@ export class CreateTournamentTeamsInputDto {
   tournamentId: number;
 
   @Field(() => [TournamentTeamInput])
-  @IsArray({ each: true })
+  @IsArray()
   teams: TournamentTeamInput[];
 }
